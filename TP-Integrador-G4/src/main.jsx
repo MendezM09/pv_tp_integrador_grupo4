528a7b8340abe85d2b117d7f365ee79ec4bf7e2a
RMaxiG <maxiguzman06@gmail.com>
2025-06-16 16:35:21 -0300

    chore: Implementacion en Main.jsx y App.jsx[rodriMG]

--- TP-Integrador-G4/src/main.jsx
+++ TP-Integrador-G4/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import { fetchProducts } from './store/productsSlice.jsx';
+import store from "./store/store.jsx"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', 
+    },
+    secondary: {
+      main: '#dc004e', 
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif', },
+});
+
+store.dispatch(fetchProducts());
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <StrictMode> 
+    <Provider store={store}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </StrictMode>
+);
