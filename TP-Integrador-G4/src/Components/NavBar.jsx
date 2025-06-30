91ccba2838903ff374fcc70f7f84d6962992520a
MarcosM09 <marcos.antonio.mendez2002@gmail.com>
2025-06-30 18:24:52 -0300

    Style: Nuevo logo y rediseño de la pagina[MarcosMendez]

--- TP-Integrador-G4/src/Components/NavBar.jsx
+++ TP-Integrador-G4/src/Components/NavBar.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import AssignmentAddIcon from '@mui/icons-material/AssignmentAdd';
-
+import logo from '../img/logo.jpg'
 function NavBar() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Tienda
-        </Typography>
-        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-          <Button color="inherit" component={Link} to="/" startIcon={<ProductionQuantityLimitsIcon />}>
+    <AppBar position="static" sx={{ backgroundColor: 'black', boxShadow: '0 2px 6px rgba(0,0,0,0.5)', }}>
+      <Toolbar sx={{ minHeight: 110, px: 3 }}>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <Box
+            component="img"
+            src={logo}
+            alt="Logo Tienda"
+            sx={{ height: 80, cursor: 'pointer', mr: 2 }}
+          />
+        </Link>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ display: { xs: 'none', sm: 'flex', gap: 2 } }}>
+          <Button color="inherit" component={Link} to="/" startIcon={<ProductionQuantityLimitsIcon />} sx={{ fontSize: '1rem', fontWeight: 500 }}>
             Productos
           </Button>
-          <Button color="inherit" component={Link} to="/favorites" startIcon={<FavoriteIcon/>}>
+          <Button color="inherit" component={Link} to="/favorites" startIcon={<FavoriteIcon />} sx={{ fontSize: '1rem', fontWeight: 500 }}>
             Favoritos
           </Button>
-          <Button color="inherit" component={Link} to="/agregarProducto" startIcon={<AssignmentAddIcon/>}>
+          <Button color="inherit" component={Link} to="/agregarProducto" startIcon={<AssignmentAddIcon />} sx={{ fontSize: '1rem', fontWeight: 500 }}>
             Agregar producto
           </Button>
         </Box>
